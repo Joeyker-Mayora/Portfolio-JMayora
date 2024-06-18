142271f93383e7c8365e74323f1d6679da05664c
Joeyker <oswaldo_avilez@outlook.es>
2024-06-18 19:37:35 -0300

    Settings

--- src/components/Proyects/Works.jsx
+++ src/components/Proyects/Works.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Comerce from '../../asset/Img/comerce.png'
 import Turnos from '../../asset/Img/turnos.png'
 import './proyects.css';
@@ -5,34 +6,69 @@ import './proyects.css';
 
 
 const Works = () => {
+  const [activeModal, setActiveModal] = useState(null);
+
+  const openModal = (index) => {
+    setActiveModal(index);
+  };
+
+  const closeModal = () => {
+    setActiveModal(null);
+  };
+
   return (
     <div className="work-container container grid">
-          <div className="work-card">
-            <img src={Turnos} alt="" className="work-img" />
-            <h3 className="work-title">Patient Management</h3>
-            <button className='work-button'>Ver mas <i class='bx bx-right-arrow-alt work-button-icon'></i></button>
-          </div>
+      <div className="work-card" onClick={() => openModal(1)}>
+        <img src={Turnos} alt="" className="work-img" />
+        <h3 className="work-title">Management Patient</h3>
+      </div>
 
-          <div className="work-card">
-            <img src={Comerce} alt="" className="work-img" />
-            <h3 className="work-title">E-commerce</h3>
-            <button className='work-button'>Ver mas <i class='bx bx-right-arrow-alt work-button-icon'></i></button>
+      <div className={activeModal === 1 ? 'modal modal-active' : 'modal'}>
+        <div className="modal-content">
+          <i onClick={closeModal} className="uil uil-times modal-close"></i>
+          <h3 className="modal-title">Management Patient</h3>
+          <img src={Turnos} alt="" className="work-img-modal" />
+          <p className="modal-description">está diseñado para optimizar la gestión de la atención médica. Con una interfaz intuitiva y fácil de usar, los profesionales de la salud pueden acceder y actualizar información de los pacientes en tiempo real.</p>
+          <div className="content-stack">
+            <p className="item-stack">Stack:
+              <span className="stack">Html</span>
+              <span className="stack">Css</span>
+              <span className="stack">Javascript</span>
+            </p>
           </div>
+          <div className="modal-link">
+            <a href="https://turnos-js.vercel.app/" className="modal-button">Demo <i className='bx bx-right-arrow-alt modal-button-icon'></i></a>
+            <a href="https://github.com/Joeyker-Mayora/TurnosJs" className="modal-button">Codigo Fuente <i className='bx bx-right-arrow-alt modal-button-icon'></i></a>
+          </div>
+        </div>
+      </div>
 
-          <div className="work-card">
-            <img src={Turnos} alt="" className="work-img" />
-            <h3 className="work-title">Patient Management</h3>
-            <button className='work-button'>Ver mas <i class='bx bx-right-arrow-alt work-button-icon'></i></button>
-          </div>
+      <div className="work-card" onClick={() => openModal(2)}>
+        <img src={Comerce} alt="" className="work-img" />
+        <h3 className="work-title">E-Commerce</h3>
+      </div>
 
-          <div className="work-card">
-            <img src={Turnos} alt="" className="work-img" />
-            <h3 className="work-title">Patient Management</h3>
-            <button className='work-button'>Ver mas <i class='bx bx-right-arrow-alt work-button-icon'></i></button>
+      <div className={activeModal === 2 ? 'modal modal-active' : 'modal'}>
+        <div className="modal-content">
+          <i onClick={closeModal} className="uil uil-times modal-close"></i>
+          <h3 className="modal-title">E-Commerce</h3>
+          <img src={Comerce} alt="" className="work-img-modal" />
+          <p className="modal-description">Está construido con un enfoque en la usabilidad y el diseño moderno. La estructura de la página permite una navegación intuitiva, con filtros avanzados y búsqueda optimizada.</p>
+          <div className="content-stack">
+            <p className="item-stack">Stack:
+              <span className="stack">Html</span>
+              <span className="stack">Css</span>
+              <span className="stack">Javascript</span>
+            </p>
           </div>
+          <div className="modal-link">
+            <a href="https://e-comerce-js-gamma.vercel.app/" className="modal-button">Demo <i className='bx bx-right-arrow-alt modal-button-icon'></i></a>
+            <a href="https://github.com/Joeyker-Mayora/E-comerceJs" className="modal-button">Codigo Fuente <i className='bx bx-right-arrow-alt modal-button-icon'></i></a>
+          </div>
+        </div>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
